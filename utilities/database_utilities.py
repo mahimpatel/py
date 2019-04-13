106a98794d945b010d76d3e0af46be522aa73519
mpatel <mpatel@lendingclub.com>
2019-04-12 18:00:11 -0700

    initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    #TODO: generate the database
+    pass
+
+def save_words_to_database(datbase_path: str, words_list: str):
+    #TODO: save the words to the database
+    pass
+
